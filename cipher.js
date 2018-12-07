edfd2541cdd6847b85f945e4554f9b872c1b8426
amdemic <amdm1982@gmail.com>
2018-12-07 08:56:36 -0300

    subiendo todo el proyecto

--- cipher.js
+++ cipher.js
@@ -1,8 +1,36 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+window.cipher = { 
+  encode: (str, offset) => {
+    let result ='';
+    offset= parseInt(offset);
+    
+    for (let i = 0; i < str.length; i++) {
+      let strToAscii = str.charCodeAt(i);
+
+      if (strToAscii >= 65 && strToAscii <= 90) {
+        result += String.fromCharCode((strToAscii - 65 + offset) % 26 + 65);
+      }
+      else {
+        result += String.fromCharCode(strToAscii);
+      } 
+      }
+    return result;
   },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+     
+  decode: (str, offset) => {
+    let result ='';
+    offset= parseInt(offset);
+    
+    
+    for (let i = 0; i < str.length; i++) {
+      let strToAscii = str.charCodeAt(i);
+
+      if (strToAscii >= 65 && strToAscii <= 90) {
+        result += String.fromCharCode((strToAscii - 65 - offset) % 26 + 65);
+      }
+      else {
+        result += String.fromCharCode(strToAscii);
+      }
+      }
+    return result;
+  },
+}
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-/* Acá va tu código */
+window.onload = () => {
+
+    const root = document.getElementById("root");
+
+    
+    document.getElementById("button").addEventListener("click",() =>{
+    let str = document.getElementById("textarea").value;
+    let offset = document.getElementById("offset").value;
+    const result = window.cipher.encode(str,offset);
+    root.innerHTML = result;
+
+});
+document.getElementById("button1").addEventListener("click",() =>{
+let str = document.getElementById("textarea").value;
+let offset = document.getElementById("offset").value;
+const result = window.cipher.decode(str,offset);
+root.innerHTML = result;
+
+});
+
+
+}
