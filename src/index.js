a44655d5a567249bd35f16676ead446fbe3e6a97
amdemic <amdm1982@gmail.com>
2018-12-11 19:40:32 -0300

    se realizan cambios de imagen de fondo y color de fuentes asi como tambien se reduce la informacion en pantalla

--- src/index.js
+++ src/index.js
@@ -2,29 +2,29 @@ window.onload = () => {
 
     const root = document.getElementById("root");
 
-    
-    document.getElementById("button").addEventListener("click",() =>{
-    let str = document.getElementById("textarea").value;
-    let offset = document.getElementById("offset").value;
-    const result = window.cipher.encode(str,offset);
-    root.innerHTML = result;
 
-});
-document.getElementById("button1").addEventListener("click",() =>{
-let str = document.getElementById("textarea").value;
-let offset = document.getElementById("offset").value;
-const result = window.cipher.decode(str,offset);
-root.innerHTML = result;
+    document.getElementById("button").addEventListener("click", () => {
+        let str = document.getElementById("textarea").value;
+        let offset = document.getElementById("offset").value;
+        const result = window.cipher.encode(str, offset);
+        root.innerHTML = result;
 
-});
+    });
+    document.getElementById("button1").addEventListener("click", () => {
+        let str = document.getElementById("textarea").value;
+        let offset = document.getElementById("offset").value;
+        const result = window.cipher.decode(str, offset);
+        root.innerHTML = result;
 
-document.getElementById("recargarpagina").addEventListener("click",() =>{
- document.getElementById("textarea").value= "";
- document.getElementById("offset").value= "";
- document.getElementById("root").innerHTML= "";
- 
+    });
 
-});
+    document.getElementById("recargarpagina").addEventListener("click", () => {
+        document.getElementById("textarea").value = "";
+        document.getElementById("offset").value = "";
+        document.getElementById("root").innerHTML = "";
+
+
+    });
 
 
 }
